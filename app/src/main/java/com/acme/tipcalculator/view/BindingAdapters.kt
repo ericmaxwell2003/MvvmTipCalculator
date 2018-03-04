e3714a4c746a1efc2ca2ccf4bbd2bd518c1467a1
Eric Maxwell <ericmaxwell2003@gmail.com>
2018-03-04 07:33:58 -0500

    Setup Lab instructions and stubs

--- app/src/main/java/com/acme/tipcalculator/view/BindingAdapters.kt
+++ app/src/main/java/com/acme/tipcalculator/view/BindingAdapters.kt
@@ -2,14 +2,32 @@ package com.acme.tipcalculator.view
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
-import android.databinding.BindingAdapter
 import android.view.View
-import android.widget.TextView
 
+/**
+ * Lab 4: Adapters and Converters
+ *
+ * Wire this function up as a Binding Adapter for TextViews to support the custom
+ * View Attribute pair
+ *
+ * app:text - A nullable String?
+ * app:animate - A Boolean
+ *
+ * After defining the proper signature with any required annotations
+ * be sure to uncomment the body of this function and make use in `content_tip_calculator.xml`.
+ *
+ * You should not need to change the body of this function or any of the private functions
+ * in this file to get the animation / visibility functionality to work.
+ *
+ * Bonus Questions:
+ * - Will this work for EditText views too?
+ * - How about ImageViews?
+ * - What would it take to apply this to all Views?
+ * - What happens if you change the order of the arguments?
+ */
+fun magicTextWithFade(/* Hint, this method needs 3 arguments.  Remember Order is important! */) {
 
-@BindingAdapter(value = ["app:text", "app:animate"])
-fun magicTextWithFade(tv: TextView, text: String?, animate: Boolean) {
-
+    /*
     if (text.isNullOrEmpty()) {
         hideView(tv, animate) {
             tv.text = ""
@@ -18,6 +36,7 @@ fun magicTextWithFade(tv: TextView, text: String?, animate: Boolean) {
         tv.text = text
         showView(tv, animate)
     }
+    */
 }
 
 private fun showView(v: View, animate: Boolean, then: () -> Unit = {}) {
