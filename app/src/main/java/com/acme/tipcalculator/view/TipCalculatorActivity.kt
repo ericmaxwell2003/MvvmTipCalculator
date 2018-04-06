96e4f977cc1921e68df6c0d21921a161d2210661
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2018-04-06 05:23:54 -0400

    Ready for binding inputs.

--- app/src/main/java/com/acme/tipcalculator/view/TipCalculatorActivity.kt
+++ app/src/main/java/com/acme/tipcalculator/view/TipCalculatorActivity.kt
@@ -1,18 +1,25 @@
 package com.acme.tipcalculator.view
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.acme.tipcalculator.R
+import com.acme.tipcalculator.databinding.ActivityTipCalculatorBinding
+import com.acme.tipcalculator.viewmodel.CalculatorViewModel
 
 class TipCalculatorActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityTipCalculatorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_tip_calculator)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_tip_calculator)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.vm = CalculatorViewModel()
+
+        setSupportActionBar(binding.toolbar)
 
     }
 
